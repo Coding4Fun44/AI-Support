c62e6d30e7f09e0aff9aed44e489e5dbe6805b86
Awab Masroor <53250798+secretstf@users.noreply.github.com>
2024-08-11 19:31:13 -0400

    added deleteIcon and styling

--- app/page.js
+++ app/page.js
@@ -1,18 +1,31 @@
-'use client'
+"use client";
 require("dotenv").config();
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function Home() {
+  const defaultMessages = [
+    {
+      role: "assistant",
+      content:
+        "Hi, I'm your personal support agent. How may I assist you today?",
+    },
+  ];
   const [messages, setMessages] = useState([
     {
       role: "assistant",
       content:
-        "Hi, I'm the Headstarter Support Agent. How may I assist you today?",
+        "Hi, I'm your personal support agent. How may I assist you today?",
     },
   ]);
 
   const [message, setMessage] = useState("");
+
+  const clearMessages = async () => {
+    setMessage("");
+    setMessages(defaultMessages);
+  };
 
   const sendMessage = async () => {
     setMessage("");
@@ -20,7 +33,7 @@ export default function Home() {
     setMessages((messages) => [
       ...messages,
       { role: "user", content: message },
-      { role: "assistant", content: "" },
+      { role: "assistant", content: "Typing..." },
     ]);
 
     const response = await fetch("/api/chat/headstarter", {
@@ -30,7 +43,6 @@ export default function Home() {
       },
       body: JSON.stringify([...messages, { role: "user", content: message }]),
     }).then(async (res) => {
-
       const reader = res.body.getReader();
       let decoder = new TextDecoder();
 
@@ -43,10 +55,13 @@ export default function Home() {
         const text = decoder.decode(value || new Uint8Array(), {
           stream: true,
         });
+
         setMessages((messages) => {
           let lastMessage = messages[messages.length - 1];
+          if (lastMessage.content === "Typing...") {
+            lastMessage.content = "";
+          }
           let otherMessages = messages.slice(0, messages.length - 1);
-          console.log(messages);
 
           return [
             ...otherMessages,
@@ -67,24 +82,83 @@ export default function Home() {
       width="100vw"
       height={"100vh"}
       display={"flex"}
-      flexDirection={"column"}
+      sx={{ flexDirection: { xs: "column", md: "row" } }}
       justifyContent={"center"}
       alignItems={"center"}
     >
+      <Box
+        display={"flex"}
+        alignItems={""}
+        p={2}
+        pr={4}
+        flexDirection={"column"}
+        sx={{ textAlign: { xs: "center", md: "left" } }}
+        maxWidth={"500px"}
+      >
+        <Typography variant={"h4"}>Chatbot v1</Typography>
+        <Typography variant={"subtitle1"} color={"#D0D0D0"}>
+          Powered by OpenAI
+        </Typography>
+        <Typography variant={"body1"} color={"#696969"}>
+          <br></br>Hi! I'm the HeadstarterAI Chatbot. I can help you with any
+          questions you have about the Headstarter program, math questions, or
+          writing help.
+        </Typography>
+
+        <Typography variant={"body1"} color={"#696969"}>
+          <br></br>Examples of questions you can ask me:
+        </Typography>
+        <Typography
+          variant={"body1"}
+          color={"#696969"}
+          marginInline={2}
+          marginTop={1}
+          marginBottom={2}
+        >
+          {
+            ['What is HeadstarterAI about?','How does HeadstarterAI work?','What is 4005 + 23323?','What is the x-intercept for y = x^2 - 4?','How can I make the following text more professional?'].map((question, index) => {
+              return <li key={index}>{question}</li>
+            })
+          }
+        </Typography>
+
+        <Typography variant={"body1"} color={"#696969"}>
+          Ask me anything!
+        </Typography>
+      </Box>
       <Stack
         direction="column"
         width={"600px"}
         height={"700px"}
         border={"1px solid black"}
-        p={2}
+        boxShadow={"rgba(0, 0, 0, 0.35) 0px 5px 15px;"}
+        borderRadius={"15px"}
         spacing={2}
       >
+        <Stack
+          bgcolor={"lightgray"}
+          height={"50px"}
+          width={"100%"}
+          display={"flex"}
+          borderRadius={"15px 15px 0 0"}
+          justifyContent={"center"}
+          spacing={2}
+          pr={0.5}
+        >
+          <Box display={"flex"} justifyContent={"flex-end"}>
+            <Button onClick={clearMessages} sx={{ borderRadius: 16 }}>
+              <DeleteIcon />
+            </Button>
+          </Box>
+        </Stack>
         <Stack
           direction={"column"}
           spacing={2}
           flexGrow={1}
           overflow={"auto"}
           maxHeight={"100%"}
+          px={2}
+          py={1}
         >
           {messages.map((message, index) => (
             <Box
@@ -101,23 +175,41 @@ export default function Home() {
                     : "secondary.main"
                 }
                 color={"white"}
-                p={3}
-                borderRadius={16}
+                width={"95%"}
+                p={2}
+                borderRadius={5}
               >
-                <Typography whiteSpace={'pre-wrap'}>{message.content}</Typography>
+                <Typography whiteSpace={"pre-wrap"}>
+                  {message.content}
+                </Typography>
               </Box>
             </Box>
           ))}
         </Stack>
-        <Stack direction={"row"} spacing={2}>
+        <Stack direction={"row"} spacing={2} p={2}>
           <TextField
             label={"Message"}
             fullWidth
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             variant={"outlined"}
+            InputProps={{
+              style: {
+                borderRadius: 16,
+              },
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
           />
-          <Button variant={"contained"} color={"primary"} onClick={sendMessage}>
+          <Button
+            variant={"contained"}
+            color={"primary"}
+            onClick={sendMessage}
+            sx={{ borderRadius: 16, textTransform: "none" }}
+          >
             Send
           </Button>
         </Stack>
